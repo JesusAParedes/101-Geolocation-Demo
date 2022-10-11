e878f71be445ffb61d1e655907497fa32de40135
JesusAParedes <paredesjesus93@gmail.com>
2022-10-10 20:46:26 -0500

    added in timeout function and error codes

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const myLocation = () => {
   console.log("Long:" + deviceLongitude)
   console.log("Lat:" + deviceLatitude)
   if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition)
+    navigator.geolocation.watchPosition(showPosition, failedPosition, {timeout:0})
   } else {
     displayCoordinates.innerHTML = "Your location is blocked. Please enable location permission."
   }
@@ -22,3 +22,21 @@ const showPosition = (coordinates) => {
 
   displayCoordinates.innerHTML =  "Longitude:  " + deviceLongitude + "<br/>  Latitude: " + deviceLatitude;
 }
+
+const failedPosition = (err) => {
+  switch(err.code)
+  {
+    case err.PERMISSION_DENIED: alert("User refused to share geolocation data"); 
+    break;
+
+    case err.POSITION_UNAVAILABLE: alert("Current position is unavailable"); 
+    break;
+
+    case err.TIMEOUT: alert("Timed out"); console.log('does it work') 
+    break;
+
+    default: alert("Unknown error"); 
+    break; 
+  }
+ }
+
